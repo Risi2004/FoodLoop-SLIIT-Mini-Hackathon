a3acc756074ac41babeb3dafa64a4c734917e95c
Risi2004 <risikesanjegatheesan@gmail.com>
2026-09-04 09:35:03 +0530

    project structure

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is running successfully 🚀",
+  });
+});
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
